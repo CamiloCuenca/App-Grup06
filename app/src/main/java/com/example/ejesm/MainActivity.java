649bb5eec20d0c79dc28fcd5b8f82f84fe177056
CamiloCuenca <88161476+CamiloCuenca@users.noreply.github.com>
2021-11-19 21:45:12 -0500

    Se realizo la conexion a la base de datos en firbase y se modifico tanto el activity del login y del registro agregando tamben al proyecto un logo

--- app/src/main/java/com/example/ejesm/MainActivity.java
+++ app/src/main/java/com/example/ejesm/MainActivity.java
@@ -5,17 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button IniciarBTN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        IniciarBTN = findViewById(R.id.IniciarBTN);
+
+
+        IniciarBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,HomeActivity.class));
+            }
+        });
+
     }
-    public void abrir (View a){
-        Intent abriractmodi = new Intent(MainActivity.this,Registro.class);
-        startActivity(abriractmodi);
-    }
+
 }
